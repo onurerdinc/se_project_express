1f89eb37d9639563565de605f50eba7eeaccdb74
Onur Erdinc <ONURERDNC@GMAIL.COM>
2024-10-18 20:03:50 -0400

    add like/unlike item

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -66,8 +66,56 @@ const deleteItem = (req, res) => {
     });
 };
 
+const likeItem = (req, res) => {
+  console.log(req.params.itemId);
+
+  ClothingItem.findByIdAndUpdate(
+    req.params.itemId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true }
+  )
+    .orFail()
+    .then((item) => res.status(200).send(item))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(NOT_FOUND).send({ message: err.message });
+      }
+      if (err.name === "CastError") {
+        return res.status(BAD_REQUEST).send({ message: err.message });
+      }
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: "An error has occurred on the server " });
+    });
+};
+
+const unlikeItem = (req, res) => {
+  ClothingItem.findByIdAndUpdate(
+    req.params.itemId,
+    { $pull: { likes: req.user._id } },
+    { new: true }
+  )
+    .orFail()
+    .then(() => res.status(200).send({ message: "unlike successfully" }))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(NOT_FOUND).send({ message: err.message });
+      }
+      if (err.name === "CastError") {
+        return res.status(BAD_REQUEST).send({ message: "Invalid data" });
+      }
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: "An error has occurred on the server " });
+    });
+};
+
 module.exports = {
-  getItems,
   createItem,
+  getItems,
   deleteItem,
+  likeItem,
+  unlikeItem,
 };
--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -3,9 +3,14 @@ const {
   getItems,
   createItem,
   deleteItem,
+  likeItem,
+  unlikeItem,
 } = require("../controllers/clothingItems");
 
 router.get("/", getItems);
 router.post("/", createItem);
 router.delete("/:itemId", deleteItem);
+router.put("/:itemId/likes", likeItem);
+router.delete("/:itemId/likes", unlikeItem);
+
 module.exports = router;
