67d828bc714011e97ac81723dad5ec1ba0f8b293
Onur Erdinc <ONURERDNC@GMAIL.COM>
2024-11-02 20:47:52 -0400

    add middleware for authorization

---
+++ middlewares/auth.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../utils/config");
+const { AUTHENTICATIONERROR } = require("../utils/errors");
+
+const auth = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return res
+      .status(AUTHENTICATIONERROR)
+      .send({ message: "Authorization required" });
+  }
+
+  const token = authorization.replace("Bearer ", "");
+  let payload;
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    return res
+      .status(AUTHENTICATIONERROR)
+      .send({ message: "Authorization required" });
+  }
+  req.user = payload;
+  return next();
+};
+
+module.exports = { auth };
