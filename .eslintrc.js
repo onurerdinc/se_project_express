cf9ce1b86ac1bdad93a808385545bc0cf0adbc51
Onur Erdinc <onurerdnc@gmail.com>
2024-11-30 19:39:55 -0500

    .

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,5 +24,6 @@ module.exports = {
   rules: {
     "no-console": ["error", { allow: ["warn", "error", "log"] }],
     "no-underscore-dangle": ["error", { allow: ["_id"] }],
+    "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
   },
 };
