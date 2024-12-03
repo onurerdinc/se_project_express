044e40953a392e6e0f346ffaaadb18c4ccc3f3b0
Onur Erdinc <onurerdnc@gmail.com>
2024-12-02 19:51:37 -0500

    .

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const { errors } = require("celebrate");
 const mainRouter = require("./routes/index");
 const errorHandler = require("./middlewares/errorHandler");
-const { errors } = require("celebrate");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
+
 mongoose.set("strictQuery", true);
 
 const app = express();
